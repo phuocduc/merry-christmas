dd543ef9c1c7dd7fee32c7763385ed84f825f991
Nguyen Phuoc Duc <phuocduc2012@gmail.com>
2020-11-08 23:08:09 +0700

    update

---
+++ js/script_music.js
@@ -0,0 +1,15 @@
+// var audio = document.getElementById("audio");
+// // var img = document.getElementById("img");
+// audio.onplaying = function () {
+//   audio.classList.add("play");
+// //   img.classList.add("hue");
+// };
+
+// audio.onpause = function () {
+//   audio.classList.remove("play");
+// //   img.classList.remove("hue");
+// };
+
+window.onload = function() {
+    document.getElementById("audio").play();
+}
